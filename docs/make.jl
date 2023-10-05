3361e54e79c3c73017e70d6a3c35836837c15942
Randall Boyes <33524191+rdboyes@users.noreply.github.com>
2023-10-05 12:15:13 -0400

    trying to fix documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,34 +1,15 @@
-using Documenter, DocumenterMarkdown
-using TidierData, DataFrames, PalmerPenguins, TidierPlots
+push!(LOAD_PATH,"../src/")
 
-DocTestMeta = quote
-    using TidierPlots, DataFrames, Chain, Statistics
-end
-DocMeta.setdocmeta!(TidierPlots,
-    :DocTestSetup,
-    DocTestMeta;
-    recursive=true)
+using Documenter
+using TidierPlots
+
 makedocs(
-    modules=[TidierPlots],
-    clean=true,
-    doctest=true,
-    #format   = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    sitename="TidierPlots.jl",
-    authors="Randall Boyes et al.",
-    strict=[
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block,
-        # :footnote, :meta_block, :missing_docs, :setup_block
-    ], checkdocs=:all, format=Markdown(), draft=false,
-    build=joinpath(@__DIR__, "docs")
+    sitename = "TidierPlots",
+    format = Documenter.HTML(),
+    modules = [TidierPlots]
 )
 
-deploydocs(; devurl="latest", repo="https://github.com/TidierOrg/TidierPlots.jl", push_preview=true,
-    deps=Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-material",
-        "pymdown-extensions", "mkdocstrings", "mknotebooks",
-        "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2", "mkdocs-video"),
-    make=() -> run(`mkdocs build`), target="site", devbranch="main")
+
+deploydocs(
+    repo = "https://github.com/TidierOrg/TidierPlots.jl.git"
+)

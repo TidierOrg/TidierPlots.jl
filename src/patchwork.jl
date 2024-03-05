1473747a60d6afe6fe403b136099e732137a319f
Randall Boyes <33524191+rdboyes@users.noreply.github.com>
2024-03-05 16:31:17 -0500

    Update patchwork.jl

--- src/patchwork.jl
+++ src/patchwork.jl
@@ -1,5 +1,6 @@
 # functions to implement the functionality of R's {patchwork} package
 
+# + -> side by side
 function Base.:+(plot1::GGPlot, plot2::GGPlot)
     return GGPlotGrid(
         [plot1, plot2],
@@ -7,6 +8,15 @@ function Base.:+(plot1::GGPlot, plot2::GGPlot)
     )
 end
 
+# | -> side by side
+function Base.:|(plot1::GGPlot, plot2::GGPlot)
+    return GGPlotGrid(
+        [plot1, plot2],
+        SpecApi.GridLayout([Makie.SpecApi.Axis(plot1) Makie.SpecApi.Axis(plot2)])
+    )
+end
+
+# / -> p1 above p2
 function Base.:/(plot1::GGPlot, plot2::GGPlot)
     return GGPlotGrid(
         [plot1, plot2],

17c2283c886f8ba3d78d66c3c4489d1438c8b1c5
Randy Boyes <rboyes@presagegroup.com>
2024-03-12 09:22:18 -0400

    adds tests for labs

--- test/runtests.jl
+++ test/runtests.jl
@@ -87,6 +87,87 @@ end
     @test plot_images_equal(t, m)
 end
 
+@testset "labs" begin
+    t = ggplot(penguins) + 
+        geom_point(@aes(x = bill_length_mm, y = bill_depth_mm))
+
+    m_title = Makie.plot(
+        Makie.SpecApi.GridLayout(
+            Makie.SpecApi.Axis(
+                plots = [
+                    Makie.PlotSpec(
+                        :Scatter, 
+                        penguins.bill_length_mm,
+                        penguins.bill_depth_mm)
+                ]; title = "Title"
+            )
+        )
+    )
+
+    @test plot_images_equal(t + labs(title = "Title"), m_title)
+
+    m_xlab = Makie.plot(
+        Makie.SpecApi.GridLayout(
+            Makie.SpecApi.Axis(
+                plots = [
+                    Makie.PlotSpec(
+                        :Scatter, 
+                        penguins.bill_length_mm,
+                        penguins.bill_depth_mm)
+                ]; xlabel = "X"
+            )
+        )
+    )
+
+    @test plot_images_equal(t + labs(x = "X"), m_xlab)
+
+    m_ylab = Makie.plot(
+        Makie.SpecApi.GridLayout(
+            Makie.SpecApi.Axis(
+                plots = [
+                    Makie.PlotSpec(
+                        :Scatter, 
+                        penguins.bill_length_mm,
+                        penguins.bill_depth_mm)
+                ]; ylabel = "Y"
+            )
+        )
+    )
+
+    @test plot_images_equal(t + labs(y = "Y"), m_ylab)
+
+    m_xylab = Makie.plot(
+        Makie.SpecApi.GridLayout(
+            Makie.SpecApi.Axis(
+                plots = [
+                    Makie.PlotSpec(
+                        :Scatter, 
+                        penguins.bill_length_mm,
+                        penguins.bill_depth_mm)
+                ]; xlabel = "X", ylabel = "Y"
+            )
+        )
+    )
+
+    @test plot_images_equal(t + labs(x = "X", y = "Y"), m_xylab)
+
+    m_xylab_title = Makie.plot(
+        Makie.SpecApi.GridLayout(
+            Makie.SpecApi.Axis(
+                plots = [
+                    Makie.PlotSpec(
+                        :Scatter, 
+                        penguins.bill_length_mm,
+                        penguins.bill_depth_mm)
+                ]; xlabel = "X", ylabel = "Y", title = "Title"
+            )
+        )
+    )
+
+    @test plot_images_equal(t + labs(x = "X", y = "Y", title = "Title"), m_xylab_title)
+
+end
+
 @testset "geom_bar" begin
     t = ggplot(penguins) + 
         geom_bar(@aes(x = species))
@@ -203,7 +284,7 @@ end
     @test plot_images_equal(t, m)
 end
 
-@testset "geom_histogram" begin
+@testset "geom_hist" begin
     t = ggplot(penguins) + 
         geom_histogram(@aes(x = bill_length_mm))
     

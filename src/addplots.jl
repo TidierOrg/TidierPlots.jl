27560af07600d06fed50c8a65b463b4f11a6bf95
Randy Boyes <rboyes@presagegroup.com>
2024-03-23 12:01:42 -0400

    comments on add plots

--- src/addplots.jl
+++ src/addplots.jl
@@ -17,13 +17,13 @@ function Base.:+(x::GGPlot, y...)::GGPlot
     end
     
     result = GGPlot(
-        vcat(x.geoms, 
+        vcat(x.geoms, # if there are geoms or lists of geoms, append them to the ggplot's geoms
             [i              for i in y if i isa Geom], 
             [i              for i in y if i isa Vector{Geom}]...), 
-        merge(x.default_aes, 
+        merge(x.default_aes, # if there are aes specs, make them the ggplot's defaults
             [i.aes          for i in y if i isa Aesthetics]...),
-        x.data,
-        merge(x.axis_options, 
+        x.data, # the default data is passed on to the final ggplot
+        merge(x.axis_options, # get any axis options out of the geoms into one location
             [i.axis_options for i in y if i isa Geom]...,
             [get_options(i) for i in y if i isa Vector{Geom}]...,
             [i.opt          for i in y if i isa AxisOptions]...),

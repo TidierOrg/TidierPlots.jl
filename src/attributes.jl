fa7e97a8ce8db1f41a28bc27d8ba79579a8dcb04
aknudson <alexk.706@gmail.com>
2024-04-16 16:34:48 -0700

    attributes.jl now only contains dictionaries of default mappings

--- src/attributes.jl
+++ src/attributes.jl
@@ -1,3 +1,18 @@
+const _ggplot_to_makie = Dict{String, String}(
+    "colour" => "color",
+    "shape" => "marker",
+    "size" => "markersize",
+    "stroke" => "strokewidth",
+    "strokecolour" => "strokecolor",
+    "linetype" => "linestyle",
+    "glow" => "glowwidth",
+    "glowcolour" => "glowcolor",
+    "errorbar_direction" => "direction",
+    "label" => "text",
+    "palette" => "colormap"
+);
+
+
 const _makie_expected_type = Dict{String, Type}(
     # Generic Attributes
     "depth_shift" => Float32,
@@ -84,114 +99,3 @@ const _makie_expected_type = Dict{String, Type}(
     "ymin" => Real,
     "ymax" => Real,
 );
-
-"""
-    MakieExpectedTypeDict <: AbstractDict{String, Type}
-
-A dictionary defining the expected types for Makie attributes with the default value `Any`.
-
-The singleton type is [`makie_expected_type`](@ref).
-
-# Examples
-
-```julia-repl
-julia> makie_expected_type("marker")
-Symbol
-
-julia> makie_expected_type("unknown_attr")
-Any
-
-julia> special_aes = Dict("unknown_attr" => Float64);
-
-julia> new_mapping = merge(makie_expected_type, special_aes);
-
-julia> new_mapping("unknown_attr")
-Float64
-
-julia> makie_expected_type("unknown_attr")
-Any
-```
-"""
-struct MakieExpectedTypeDict <: AbstractDict{String, Type}
-    dict::AbstractDict{String, Type}
-end
-
-Base.keys(d::MakieExpectedTypeDict) = keys(d.dict)
-Base.values(d::MakieExpectedTypeDict) = values(d.dict)
-Base.iterate(d::MakieExpectedTypeDict, state=1) = iterate(d.dict, state)
-Base.length(d::MakieExpectedTypeDict) = length(d.dict)
-Base.get(d::MakieExpectedTypeDict, key, default=Any) = get(d.dict, key, default)
-Base.getindex(d::MakieExpectedTypeDict, key) = get(d.dict, key, Any)
-
-(d::MakieExpectedTypeDict)(key::String) = d[key]
-
-function Base.merge(d::MakieExpectedTypeDict, others::AbstractDict{String, Type}...)
-    new_dict = merge(d.dict, others...)
-    return MakieExpectedTypeDict(new_dict)
-end
-
-"""
-    makie_expected_type
-
-The singleton instance of type [`MakieExpectedTypeDict`](@ref). b
-"""
-const makie_expected_type = MakieExpectedTypeDict(_makie_expected_type)
-
-
-
-const _ggplot_to_makie_attributes = Dict{String, String}(
-    "colour" => "color",
-    "shape" => "marker",
-    "size" => "markersize",
-    "stroke" => "strokewidth",
-    "strokecolour" => "strokecolor",
-    "linetype" => "linestyle",
-    "glow" => "glowwidth",
-    "glowcolour" => "glowcolor",
-    "errorbar_direction" => "direction",
-    "label" => "text",
-    "palette" => "colormap"
-)
-
-"""
-    GGPlotToMakieAttributeDict <: AbstractDict{String, String}
-
-A dictionary defining translations from GGPlot attributes to Makie attributes. Default value
-is just the key.
-
-The singleton instance is [`ggplot_to_makie_attribute`](@ref).
-
-# Examples
-
-```julia-repl
-julia> ggplot_to_makie_attribute["colour"]
-"color"
-
-julia> ggplot_to_makie_attribute["orientation"]
-"orientation"
-```
-"""
-struct GGPlotToMakieAttributeDict <: AbstractDict{String, String}
-    dict::AbstractDict{String, String}
-end
-
-Base.keys(d::GGPlotToMakieAttributeDict) = keys(d.dict)
-Base.values(d::GGPlotToMakieAttributeDict) = values(d.dict)
-Base.iterate(d::GGPlotToMakieAttributeDict, state=1) = iterate(d.dict, state)
-Base.length(d::GGPlotToMakieAttributeDict) = length(d.dict)
-Base.get(d::GGPlotToMakieAttributeDict, key, default=key) = get(d.dict, key, default)
-Base.getindex(d::GGPlotToMakieAttributeDict, key) = get(d.dict, key, key)
-
-(d::GGPlotToMakieAttributeDict)(key::String) = d[key]
-
-function Base.merge(d::GGPlotToMakieAttributeDict, others::AbstractDict{String, String}...)
-    new_dict = merge(d.dict, others...)
-    return GGPlotToMakieAttributeDict(new_dict)
-end
-
-"""
-    ggplot_to_makie_attribute
-
-The singleton instance of type [`GGPlotToMakieAttributeDict`](@ref).
-"""
-const ggplot_to_makie_attribute = GGPlotToMakieAttributeDict(_ggplot_to_makie_attributes)

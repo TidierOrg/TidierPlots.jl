d526855619d722e242d175a5bc0da336abc2f799
Connor Robertson <cnr.robertson@gmail.com>
2024-04-26 08:31:08 -0700

    add ggsave for GGPlotGrid

--- src/ggsave.jl
+++ src/ggsave.jl
@@ -1,5 +1,5 @@
 function ggsave(path::String, plot::GGPlot; scale = 2)
-    save(path, 
+    save(path,
     with_theme(plot.theme) do
         draw_ggplot(plot)
     end;
@@ -7,9 +7,15 @@ function ggsave(path::String, plot::GGPlot; scale = 2)
 end
 
 function ggsave(plot::GGPlot, path::String; scale = 2)
-    save(path, 
+    save(path,
         with_theme(plot.theme) do
             draw_ggplot(plot)
         end;
         px_per_unit = scale)
 end
+
+function ggsave(plot::GGPlotGrid, path::String; scale = 2)
+    save(path, draw_ggplot(plot), px_per_unit = scale)
+end
+
+ggsave(path::String, plot::GGPlotGrid; scale = 2) = ggsave(plot, path; scale=scale)

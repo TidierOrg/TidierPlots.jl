b5e1f640efe863a2891b261ab6672b3567ee5ec6
Randy Boyes <rboyes@presagegroup.com>
2025-01-07 10:56:42 -0500

    fix density again

--- docs/examples/geoms/geom_density.jl
+++ docs/examples/geoms/geom_density.jl
@@ -12,4 +12,4 @@ ggplot(penguins, @aes(x=bill_length_mm)) +
 #-
 
 ggplot(penguins, @aes(x=bill_length_mm)) +
-    geom_density(color = (:red, 0.3), strokecolor = :red, stroke = 2)
+    geom_density(color = :black, stroke = 2)

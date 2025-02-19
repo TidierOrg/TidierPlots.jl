827e5c870ed5e3b93de579d79e694140daaf85de
Randy Boyes <rboyes@presagegroup.com>
2025-02-19 10:52:09 -0500

    fixes text examples

--- docs/examples/geoms/geom_text.jl
+++ docs/examples/geoms/geom_text.jl
@@ -2,21 +2,21 @@ using TidierPlots
 using DataFrames
 
 df = DataFrame(
-    x = [1,1,2,2],
-    y = [1,2,1,2],
-    t = ["A", "B", "C", "D"]
+    x=[1, 1, 2, 2],
+    y=[1, 2, 1, 2],
+    t=["A", "B", "C", "D"]
 )
 
-ggplot(df, @aes(x=x, y=y, text=t, color=t)) + geom_text()
+ggplot(df, @aes(x = x, y = y, text = t, color = t)) + geom_text()
 
-ggplot(df, @aes(x=x, y=y, text=t, color=t)) +
-    geom_text(fontsize=24, align=(:left, :bottom), font=:bold) +
-    geom_point() +
-    lims(x = (0, 3), y = (0, 3))
+ggplot(df, @aes(x = x, y = y, color = t)) +
+geom_text(@aes(text = t), fontsize=24, align=(:left, :bottom), font=:bold) +
+geom_point() +
+lims(x=(0, 3), y=(0, 3))
 
-ggplot(df, @aes(x=x, y=y, text=t, color=t)) + geom_label()
+ggplot(df, @aes(x = x, y = y, text = t, color = t)) + geom_label()
 
-ggplot(df, @aes(x=x, y=y, text=t, color=t)) +
-    geom_label(fontsize=24, align=(:left, :bottom), font=:bold) +
-    geom_point() +
-    lims(x = (0, 3), y = (0, 3))
+ggplot(df, @aes(x = x, y = y, color = t)) +
+geom_label(@aes(text = t), fontsize=24, align=(:left, :bottom), font=:bold) +
+geom_point() +
+lims(x=(0, 3), y=(0, 3))
